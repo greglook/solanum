1b82f6a9b86947866ef2dd261882439cca5228d5
Greg Look <greg@greg-look.net>
2011-05-03 04:03:29 +0000

    Simplified the way Metrics are represented internally; now it is just a map of full path name => metric object. Still some logic to ensure metric paths are always leaf nodes.

--- solanum/metrics.rb
+++ solanum/metrics.rb
@@ -7,36 +7,37 @@ require 'solanum/metrics/record'
 
 module Solanum
 
-# This class provides a top-level collection of metric records, organized into
-# a tree structure by categories.
+# This class provides a top-level collection of metric records.
 #
 # Author:: Greg Look
 class Metrics
-    attr_reader :root
+    attr_reader :metrics
     
     # Creates a new metrics collection
     def initialize
-        @root = Category.new
+        @metrics = Hash.new
+    end
+    
+    # Returns a Metric object for the given name, creating one if it does not
+    # already exist.
+    def metric(name)
+        raise "Invalid metric name: #{name}" unless name =~ /^\w[\w.]+\w$/
+        return @metrics[name] if @metrics[name]
+        
+        descendents = @metrics.keys.select {|m| m[0..name.length] == name + '.' }
+        raise "Cannot create new metric in category #{name}" unless descendents.empty?
+        
+        path = name.split('.')
+        ancestors = (0..(path.length-2)).map {|i| path[0..i].join('.') }
+        ancestor = ancestors.find {|a| @metrics[a] }
+        raise "Cannot create new metric #{name} under existing metric #{ancestor}" if ancestor
+        
+        @metrics[name] ||= Metric.new
     end
     
     # Records a new value in a metric's history
-    def record(path, value, options={})
-        return if value.nil?
-        metric = resolve path
-        metric.records << Record.new(Time.now, value, options[:unit])
-    end
-    
-    # Measures a new value for a metric, but purges any history
-    def measure(path, value, options={})
-        return if value.nil?
-        metric = resolve path
-        metric.records.clear
-        metric.records << Record.new(Time.now, value, options[:unit])
-    end
-    
-    # Resolves a path to a metric, creating it if necessary
-    def resolve(path)
-        @root.resolve(path.strip.split('.').map {|p| p.intern })
+    def record(name, value, args*)
+        metric(name).record value, args*
     end
 end
 
--- solanum/metrics/metric.rb
+++ solanum/metrics/metric.rb
@@ -3,15 +3,47 @@
 module Solanum
 class Metrics
 
-# This class represents a single metric with a latest value and a history.
+# This class represents a single metric with a timestamp and one or more
+# unit=>value pairs.
 #
 # Author:: Greg Look
 class Metric
-    attr_reader :records
+    attr_reader :time
     
     # Creates a new Metric
     def initialize
-        @records = [ ]
+        @values = Hash.new
+        @time = Time.now
+    end
+    
+    # Records a new value
+    def record(value, unit=nil)
+        @values[value_key unit] = value
+        @time = Time.now
+    end
+    
+    # Gets a value for the given unit
+    def value(unit=nil)
+        @values[value_key unit]
+    end
+    
+    # Returns a list of units this metric has values for
+    def units
+        @values.keys
+    end
+    
+    # Removes all value records
+    def clear
+        @values.clear
+    end
+    
+    private
+    
+    # Fixes user-provided units
+    def value_key(unit)
+        unit ||= '1'  # default unit of :1
+        unit = unit.intern unless unit.is_a? Symbol
+        unit
     end
 end
 
