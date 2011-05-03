9dfdba809327dd4366542140c992fdd4a3dd0754
Greg Look <greg@greg-look.net>
2011-05-03 04:09:06 +0000

    Removing support for :measure option

--- solanum/lang/input_context.rb
+++ solanum/lang/input_context.rb
@@ -23,22 +23,15 @@ class InputContext
         
         commands = 0
         commands += 1 if options[:record]
-        commands += 1 if options[:measure]
         commands += 1 if block_given?
-        raise "Must supply one of :record, :measure, or a block" if commands == 0
-        raise "Only one of :record, :measure, or a block should be provided" if commands > 1
+        raise "Must specify :record or provide a block to execute" if commands == 0
+        raise "Only one of :record or a block should be provided" if commands > 1
         
         if options[:record]
             block = lambda do |m|
                 op = options[:as] || :to_s
                 value = m[1].send op
                 record options[:record], value, :unit => options[:unit]
-            end
-        elsif options[:measure]
-            block = lambda do |m|
-                op = options[:as] || :to_s
-                value = m[1].send op
-                measure options[:measure], value, :unit => options[:unit]
             end
         end
         
