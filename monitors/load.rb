fb699af0fe8451a60a01df1b46a2c87ee38231f8
Greg Look <greg@greg-look.net>
2014-06-03 06:09:27 +0000

    Add cpu and load monitors.

---
+++ monitors/load.rb
@@ -0,0 +1,15 @@
+# Solanum monitoring configuration for cpu utilization metrics.
+#
+# Author:: Greg Look
+
+read "/proc/uptime" do
+  match /^(\d+\.\d+)/, cast: :to_f, record: 'uptime'
+end
+
+read "/proc/loadavg" do
+  match /^(\d+\.\d+) \d+\.\d+ \d+\.\d+ (\d+)\/(\d+) \d+$/ do |m, metrics|
+    metrics['process load']    = m[1].to_f
+    metrics['process running'] = m[2].to_i
+    metrics['process count']   = m[3].to_i
+  end
+end
