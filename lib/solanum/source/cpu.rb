09f39f4fe58446b268c603473f19809f49eb1561
Greg Look <greg@greg-look.net>
2017-12-20 16:32:52 -0800

    Implement CPU source.

--- lib/solanum/source/cpu.rb
+++ lib/solanum/source/cpu.rb
@@ -1,6 +1,7 @@
 require 'solanum/source'
 
 class Solanum::Source::Cpu < Solanum::Source
+  attr_reader :detailed, :per_core, :thresholds
 
   STATES = %w{user nice system idle iowait irqhard irqsoft}
 
@@ -8,32 +9,123 @@ class Solanum::Source::Cpu < Solanum::Source
   def initialize(opts)
     super(opts)
     @last = nil
-    # TODO: per_cpu option
+    @detailed = opts['detailed'] || false
+    @per_core = opts['per_core'] || false
+    @thresholds = opts['thresholds'] || {}
   end
 
 
+  # Calculate the state of a metric.
+  def event_state(metric)
+    crit_at = @thresholds['critical']
+    warn_at = @thresholds['critical']
+    if crit_at && crit_at <= usage
+      'critical'
+    elsif warn_at && warn_at <= usage
+      'warning'
+    else
+      'ok'
+    end
+  end
+
+
+  # Calculate cpu utilization from the cumulative time spent in
+  # 'jiffies' (1/100 sec) since system boot.
+  def parse_info(line)
+    fields = line.chomp.split(' ')
+    name = fields.shift
+    data = Hash.new(0)
+    STATES.each_with_index do |state, i|
+      data[state] = fields[i].to_i
+    end
+    return name, data
+  end
+
+
+  # Collect metric events.
   def collect!
-    header = File.readlines('/proc/stat').first.chomp.split(' ').drop(1)
-    current = Hash.new(0)
-    diff = Hash.new(0)
-
-    # calculate cpu utilization from the cumulative time spent in
-    # 'jiffies' (1/100 sec) since system boot
-    STATES.each_with_index do |name, i|
-      jiffies = header[i].to_i
-      current[name] = jiffies
-      diff[name] = jiffies - @last[name] if @last
+    # Parse lines from usage stats.
+    lines = File.readlines('/proc/stat').take_while {|l| l.start_with? 'cpu' }
+    totals = {}
+    lines.each do |line|
+      name, data = parse_info(line)
+      totals[name] = data
     end
 
-    diff_total = diff.values.reduce(&:+)
-    diff.keys.each do |name|
-      diff[name] = diff[name].to_f/diff_total
+    # Need at least one run to start reporting accurate metrics.
+    unless @last
+      @last = totals
+      return []
     end
 
-    @last = current
-    puts diff.inspect # DEBUG
+    # Calculate diffs from previous measurements.
+    diffs = {}
+    totals.each do |name, data|
+      diffs[name] = Hash.new(0)
+      data.each do |state, jiffies|
+        last = @last[name] && @last[name][state]
+        diffs[name][state] = jiffies - last if last
+      end
+    end
+    @last = totals
 
-    []
+    # Convert diffs to relative percentages.
+    diffs.each do |name, diff|
+      elapsed = diff.values.reduce(&:+)
+      diff.keys.each do |state|
+        diff[state] = diff[state].to_f/elapsed
+      end
+    end
+
+    # Generate metric events.
+    events = []
+
+    # aggregate cpu usage
+    usage = 1.0 - diffs['cpu']['idle']
+    events << {
+      service: 'cpu usage',
+      metric: usage,
+      state: event_state(usage),
+    }
+
+    # detailed aggregate cpu state metrics
+    if @detailed
+      diffs['cpu'].each do |state, usage|
+        events << {
+          service: 'cpu state',
+          metric: usage,
+          cpu_state: state,
+        }
+      end
+    end
+
+    # even more detailed per-core usage
+    if @per_core
+      diffs.each do |name, diff|
+        next if name == 'cpu'
+
+        usage = 1.0 - diff['idle']
+        events << {
+          service: 'cpu core usage',
+          metric: usage,
+          state: event_state(usage),
+          cpu_core: name,
+        }
+
+        if @detailed
+          diff.each do |state, usage|
+            events << {
+              service: 'cpu core state',
+              metric: usage,
+              cpu_core: name,
+              cpu_state: state,
+            }
+          end
+        end
+      end
+    end
+
+    events
   end
 
 end
