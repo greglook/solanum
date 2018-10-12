ba9f0481015c01b75179016f955b98bc542f9529
Greg Look <greg@greg-look.net>
2018-10-11 23:05:31 -0700

    Add main entry ns.

---
+++ src/solanum/main.clj
@@ -0,0 +1,60 @@
+(ns solanum.main
+  "Main entry for the daemon."
+  (:gen-class)
+  (:require
+    [clojure.java.shell :as sh]
+    [clojure.string :as str]
+    [clojure.tools.cli :as cli]
+    [clojure.tools.logging :as log]))
+
+
+(defn- load-hostname
+  "Look up the name of the local host."
+  [ctx]
+  (let [result (sh/sh "hostname")]
+    (if (zero? (:exit result))
+      (str/trim-newline (:out result))
+      (log/warn "Failed to resolve local hostname:" (pr-str (:err result))))))
+
+
+(def cli-options
+  "Command-line tool options."
+  [["-H" "--host NAME" "Metric event host name"
+    :default-fn load-hostname]
+   ["-a" "--attribute KEY=VAL" "Attribute to add to every event (may be set multiple times)"
+    :default {}
+    :default-desc ""
+    :parse-fn #(vec (str/split % #"=" 2))
+    :update-fn conj]
+   ["-t" "--tag TAG" "Tag to add to every event (may be set multiple times)"
+    :default #{}
+    :default-desc ""
+    :update-fn conj]
+   [nil "--ttl SECONDS" "Default TTL for events"
+    :parse-fn #(Integer/parseInt %)
+    :default 60]
+   ["-h" "--help"]])
+
+
+(defn -main
+  "Main entry point."
+  [& args]
+  (let [parse (cli/parse-opts args cli-options)
+        config-paths (parse :arguments)
+        options (parse :options)]
+    (when-let [errors (parse :errors)]
+      (binding [*out* *err*]
+        (run! println errors)
+        (System/exit 1)))
+    (when (or (:help options) (empty? config-paths))
+      (println "Usage: solanum [options] <config.yml> [config2.yml ...]")
+      (newline)
+      (println (parse :summary))
+      (flush)
+      (System/exit (if (:help options) 0 1)))
+    (println '(do the-thing))
+    (prn options)
+    (prn config-paths)
+    ; TODO: load and merge configs
+    ; TODO: enter scheduling loop
+    (System/exit 0)))
