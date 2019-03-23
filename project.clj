edb0cfd86a4c122153a41ddcd650433092a3ce69
Greg Look <greg@amperity.com>
2019-03-22 22:22:20 -0700

    Set minor release version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/solanum "3.1.3-SNAPSHOT"
+(defproject mvxcvi/solanum "3.2.0"
   :description "Local host monitoring daemon."
   :url "https://github.com/greglook/solanum"
   :license {:name "Public Domain"
