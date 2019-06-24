c75a17ce8e9cdf036f1ec1bdc978b7f41fc98fb2
Greg Look <greg@amperity.com>
2019-06-24 13:28:38 -0700

    Bump snapshot version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mvxcvi/solanum "3.2.1-SNAPSHOT"
+(defproject mvxcvi/solanum "3.2.3-SNAPSHOT"
   :description "Local host monitoring daemon."
   :url "https://github.com/greglook/solanum"
   :license {:name "Public Domain"
